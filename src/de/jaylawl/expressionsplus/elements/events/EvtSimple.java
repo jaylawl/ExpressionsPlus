d36cb4cb27ab44025a06a4948605524e7aaa1609
jaylawl <jaylawlmc@gmail.com>
2018-08-30 15:11:32 +0200

    Add Armor Stand Manipulate event
    + experimental events

--- src/de/jaylawl/expressionsplus/elements/events/EvtSimple.java
+++ src/de/jaylawl/expressionsplus/elements/events/EvtSimple.java
@@ -2,19 +2,21 @@ package de.jaylawl.expressionsplus.elements.events;
 
 import ch.njol.skript.Skript;
 import ch.njol.skript.lang.util.SimpleEvent;
+import org.bukkit.event.player.PlayerArmorStandManipulateEvent;
 import org.bukkit.event.player.PlayerSwapHandItemsEvent;
+import com.destroystokyo.paper.event.entity.EntityPathfindEvent;
 import com.destroystokyo.paper.event.player.PlayerJumpEvent;
-import com.destroystokyo.paper.event.block.BeaconEffectEvent;
 
 @SuppressWarnings("unchecked")
 public class EvtSimple {
     static {
-        Skript.registerEvent("Swap Hand Items", SimpleEvent.class, PlayerSwapHandItemsEvent.class, "swap [hand] item[s]");
-        if (Skript.classExists("com.destroystokyo.paper.event.player.PlayerJumpEvent")) {
-            Skript.registerEvent("Jump", SimpleEvent.class, PlayerJumpEvent.class, "jump");
+        Skript.registerEvent("Armor Stand Manipulate", SimpleEvent.class, PlayerArmorStandManipulateEvent.class, "armor( |-)stand manipulat(e|ing|ion)");
+        Skript.registerEvent("Swap Hand Items", SimpleEvent.class, PlayerSwapHandItemsEvent.class, "swap[ping of] [hand( |-)]item[s]");
+        if (Skript.classExists("com.destroystokyo.paper.event.entity.EntityPathfindEvent")) {
+            Skript.registerEvent("Entity Pathfind", SimpleEvent.class, EntityPathfindEvent.class, "entity( |-)pathfind[ing]");
         }
-        /*if (Skript.classExists("com.destroystokyo.paper.event.block.BeaconEffectEvent")) {
-            Skript.registerEvent("Beacon Tick", SimpleEvent.class, BeaconEffectEvent.class, "beacon [effect ](tick|pulse)");
-        }*/
+        if (Skript.classExists("com.destroystokyo.paper.event.player.PlayerJumpEvent")) {
+            Skript.registerEvent("Jump", SimpleEvent.class, PlayerJumpEvent.class, "jump[ing]");
+        }
     }
 }
