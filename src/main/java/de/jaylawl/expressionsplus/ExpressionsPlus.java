7a9a3463a13c2e68e1ea49f8c885acece0ee833c
Nicofisi <m.lomiak@gmail.com>
2018-08-31 21:52:34 +0200

    Make the onEnable nicer

--- src/main/java/de/jaylawl/expressionsplus/ExpressionsPlus.java
+++ src/main/java/de/jaylawl/expressionsplus/ExpressionsPlus.java
@@ -8,35 +8,35 @@ import java.io.IOException;
 
 public class ExpressionsPlus extends JavaPlugin {
 
-    private SkriptAddon addon;
-
     @Override
     public void onEnable() {
-        if ((Bukkit.getPluginManager().getPlugin("Skript") != null) && (Skript.isAcceptRegistrations())) {
-            if (!Bukkit.getBukkitVersion().contains("1.12")) {
-                getLogger().info("Warning: this plugin was designed for MC 1.12.x");
-            }
-            addon = Skript.registerAddon(this);
-            try {
-                addon.loadClasses("de.jaylawl.expressionsplus", "elements");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            if (!Skript.classExists("com.destroystokyo.paper.PaperConfig")) {
-                getLogger().info("Couldn't detect PaperSpigot");
-                getLogger().info("Some expressions might be unavailable");
-            } else {
-                getLogger().info("Activated PaperSpigot dependent contents");
-            }
-            getLogger().info("Successfully enabled");
-        } else {
+        if (Bukkit.getPluginManager().getPlugin("Skript") == null || !Skript.isAcceptRegistrations()) {
             getLogger().info("The dependency Skript is missing: disabling");
-            Bukkit.getPluginManager().disablePlugin(this);
+            setEnabled(false);
+            return;
         }
+        if (!Bukkit.getBukkitVersion().contains("1.12")) {
+            getLogger().info("Warning: this plugin was designed for MC 1.12.x");
+        }
+
+        SkriptAddon addon = Skript.registerAddon(this);
+        try {
+            addon.loadClasses("de.jaylawl.expressionsplus", "elements");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (!Skript.classExists("com.destroystokyo.paper.PaperConfig")) {
+            getLogger().info("Couldn't detect PaperSpigot");
+            getLogger().info("Some expressions might be unavailable");
+        } else {
+            getLogger().info("Activated PaperSpigot dependent contents");
+        }
+        getLogger().info("Successfully enabled");
     }
 
     @Override
     public void onDisable() {
+
     }
 
 }
