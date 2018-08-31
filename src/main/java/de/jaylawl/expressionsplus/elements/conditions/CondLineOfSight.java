664bfa4c1dd46588e13f2125576b3a1361ec4fb2
Nicofisi <m.lomiak@gmail.com>
2018-08-31 18:49:45 +0200

    Make the condition look nicer

--- src/main/java/de/jaylawl/expressionsplus/elements/conditions/CondLineOfSight.java
+++ src/main/java/de/jaylawl/expressionsplus/elements/conditions/CondLineOfSight.java
@@ -2,11 +2,13 @@ package de.jaylawl.expressionsplus.elements.conditions;
 
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.Event;
+
 import ch.njol.skript.Skript;
 import ch.njol.skript.lang.Condition;
 import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.SkriptParser.ParseResult;
 import ch.njol.util.Kleenean;
+
 import javax.annotation.Nullable;
 
 public class CondLineOfSight extends Condition {
@@ -17,31 +19,29 @@ public class CondLineOfSight extends Condition {
                 "%entity% (0¦is|1¦is(n't| not)) in %entity%'s line of sight");
     }
 
-    Expression<LivingEntity> e1;
-    Expression<LivingEntity> e2;
+    private Expression<LivingEntity> e1;
+    private Expression<LivingEntity> e2;
 
     @SuppressWarnings("unchecked")
     @Override
-    public boolean init(Expression<?>[] expressions, int matchedPattern, Kleenean isDelayed, ParseResult parser) {
-        this.e1 = (Expression<LivingEntity>) expressions[0];
-        this.e2 = (Expression<LivingEntity>) expressions[1];
+    public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parser) {
+        this.e1 = (Expression<LivingEntity>) exprs[0];
+        this.e2 = (Expression<LivingEntity>) exprs[1];
         setNegated(parser.mark == 1);
         return true;
     }
 
     @Override
-    public String toString(@Nullable Event event, boolean debug) {
-        return "has line of sight";
+    public boolean check(Event e) {
+        return e1.check(e,
+                entity1 -> e2.check(e,
+                        entity1::hasLineOfSight
+                ), isNegated());
     }
 
     @Override
-    public boolean check(Event event) {
-        LivingEntity a = e1.getSingle(event);
-        LivingEntity b = e2.getSingle(event);
-        if (a == null || b == null) {
-            return isNegated();
-        }
-        return a.hasLineOfSight(b) ? isNegated() : !isNegated();
+    public String toString(@Nullable Event e, boolean debug) {
+        return e1.toString(e, debug) + " has line of sight of " + e2.toString(e, debug);
     }
 
 }
